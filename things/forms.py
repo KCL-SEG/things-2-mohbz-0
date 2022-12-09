419c7cba292849f70557a059fa2aedc51d34f2a1
bhn-admin <amohabbat02@gmail.com>
2022-12-09 13:03:47 +0000

    push with error

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,11 @@
 """Forms of the project."""
 
 # Create your forms here.
+class ThingForm(forms.Form):
+    name = forms.CharField(widget=forms.TextInput(attrs={'name':'name'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'name':'description', 'rows':'3'}))
+    quantity = forms.IntegerField()
+
+    def clean(self):
+        if len(self.cleaned_Data['name']) == 0 or len(self.cleaned_Data['description']) == 0 or self.cleaned_Data['quantity'] == 0:
+            raise ValueError('Bad Data')
